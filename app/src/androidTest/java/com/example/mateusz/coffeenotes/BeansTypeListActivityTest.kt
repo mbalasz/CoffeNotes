367d30164e699ce120cc2f396904e5f09b99c682
Mateusz Balasz <matti93@gmail.com>
2017-07-23 14:10:02 +0100

    Abstract out common logic for ListFragment tests.

--- app/src/androidTest/java/com/example/mateusz/coffeenotes/BeansTypeListActivityTest.kt
+++ app/src/androidTest/java/com/example/mateusz/coffeenotes/BeansTypeListActivityTest.kt
@@ -1,23 +1,24 @@
 package com.example.mateusz.coffeenotes
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
-import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
 import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.IntentMatchers
+import android.support.test.espresso.intent.Intents.intending
+import android.support.test.espresso.intent.matcher.IntentMatchers.*
 import android.support.test.espresso.intent.rule.IntentsTestRule
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BeansTypeListActivityTest {
+class BeansTypeListActivityTest : AbstractListFragmentTest() {
+    val beansTypeDataManager: BeansTypeDataManager by lazy {
+        BeansTypeDataManager.instance(activityRule.activity)
+    }
 
     @Rule @JvmField
     val activityRule: ActivityTestRule<BeansTypeListActivity> =
@@ -26,15 +27,32 @@ class BeansTypeListActivityTest {
     @Before
     fun setUpBeansTypeList() {
         // TODO: Provide a test module for data manager.
-        BeansTypeDataManager.instance(activityRule.activity).saveBeansType(BeansType())
+        beansTypeDataManager.saveBeansType(BeansType())
+    }
+
+    @After
+    fun cleanUpBeansTypeList() {
+        beansTypeDataManager.getBeansTypes().forEach {
+            beansType -> beansTypeDataManager.removeBeansType(beansType)
+        }
     }
 
     @Test
     fun onMenuNewItem_startBeansTypeActivity() {
-        onView(withId(R.id.menu_item_list_start_edit)).perform(click())
+        clickView(R.id.menu_item_list_start_edit)
 
-        onView(withId(R.id.menu_item_list_new_item)).perform(click())
+        clickView(R.id.menu_item_list_new_item)
 
-        intended(IntentMatchers.hasComponent(BeansTypeActivity::class.java.name))
+        intended(hasComponent(BeansTypeActivity::class.java.name))
+    }
+
+    @Test
+    fun onBeansTypeFragmentResultOk_updateList() {
+        val activityResult = Instrumentation.ActivityResult(RESULT_OK, null)
+        intending(hasComponent(BeansTypeActivity::class.java.name)).respondWith(activityResult)
+
+        clickView(R.id.menu_item_list_start_edit)
+        clickView(R.id.menu_item_list_new_item)
+
     }
 }
